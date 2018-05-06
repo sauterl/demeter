5a0cf74ab583cf2f10eb1c19ff8cf0f190c8cf6d
loris.sauter <loris.sauter@unibas.ch>
2018-05-06 10:00:52 +0200

    Removed unused code

--- src/main/kotlin/com/github/sauterl/demeter/utils/Container.kt
+++
@@ -1,8 +0,0 @@
-package com.github.sauterl.demeter.utils
-
-/**
- * TODO: write JavaDoc
- * @author loris.sauter
- */
-
-//data class Container<T>(val contentName:String, @JsonProperty(contentName)val content : T)
