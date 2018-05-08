4471c4191ba77c0c272bb6d0cea14335801b2cdc
loris.sauter <loris.sauter@unibas.ch>
2018-05-08 09:55:13 +0200

    Added more exception safe blocks to InstagramScraper

--- src/main/kotlin/com/github/sauterl/demeter/instagram/InstagramWebScraper.kt
+++ src/main/kotlin/com/github/sauterl/demeter/instagram/InstagramWebScraper.kt
@@ -1,5 +1,6 @@
 package com.github.sauterl.demeter.instagram
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.kittinunf.fuel.Fuel
@@ -7,6 +8,7 @@ import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.result.Result
 import com.github.sauterl.demeter.utils.traceTriple
 import mu.KotlinLogging
+import java.lang.NullPointerException
 import java.net.URLEncoder
 
 /**
@@ -46,30 +48,69 @@ object InstagramWebScraper {
   }
 
   private fun parseResponse(result: Result<String, FuelError>): Pair<List<InstagramWebNode>, InstagramWebPageInfo> {
-    val mapper = jacksonObjectMapper()
-    val root = mapper.readTree(result.get())
-    val graphql = root["graphql"]
-    val hashtag = graphql["hashtag"]
-    val hashtag2media = hashtag["edge_hashtag_to_media"]
-    val pageInfo = hashtag2media["page_info"]
-    val endCursor = pageInfo["end_cursor"].asText()
-    val hasMore = pageInfo["has_next_page"].asBoolean()
-    val edges = hashtag2media["edges"]
-    val nodeList: MutableList<InstagramWebNode> = mutableListOf()
-    for (i in 0..(edges.size() - 1)) {
-      val it = edges[i]
-      val nodeJson = it["node"]
-      val nodeTxt = mapper.writeValueAsString(nodeJson)
-      val out: InstagramWebNode = mapper.readValue(nodeTxt)
-      nodeList.add(out)
+    if(result == null || (result.get().isBlank())){
+      return createEmpty()
     }
-    return Pair(nodeList.toList(), InstagramWebPageInfo(endCursor, hasMore))
+    val mapper = jacksonObjectMapper()
+    try{
+      val root = mapper.readTree(result.get())
+      val graphql = root["graphql"]
+      if(!checkForSanity(graphql,"graphql")){
+        return createEmpty()
+      }
+      val hashtag = graphql["hashtag"]
+      if(!checkForSanity(hashtag, "graphql.hashtag")){
+        return createEmpty()
+      }
+      val hashtag2media = hashtag["edge_hashtag_to_media"]
+      if(!checkForSanity(hashtag2media, "graphql.hashtag.edge_hashtag_to_media")){
+        return createEmpty()
+      }
+      val pageInfo = hashtag2media["page_info"]
+
+      val endCursor = pageInfo["end_cursor"].asText()
+      val hasMore = pageInfo["has_next_page"].asBoolean()
+      val edges = hashtag2media["edges"]
+      if(!checkForSanity(edges, "graphql.hashtag.edge_hashtag_to_media.edges")){
+        return createEmpty()
+      }
+      val nodeList: MutableList<InstagramWebNode> = mutableListOf()
+      for (i in 0..(edges.size() - 1)) {
+        val it = edges[i]
+        val nodeJson = it["node"]
+        val nodeTxt = mapper.writeValueAsString(nodeJson)
+        val out: InstagramWebNode = mapper.readValue(nodeTxt)
+        nodeList.add(out)
+      }
+      return Pair(nodeList.toList(), InstagramWebPageInfo(endCursor, hasMore))
+    }catch (e:NullPointerException){
+      logger.error { "An exception with message ${e.message} and stacktrace ${e.stackTrace} occurred." }
+      return createEmpty()
+    }
+
+  }
+
+  private fun createEmpty():Pair<List<InstagramWebNode>,InstagramWebPageInfo>{
+    return Pair(emptyList(), InstagramWebPageInfo.empty)
+  }
+
+  private fun checkForSanity(node:JsonNode?, name:String):Boolean{
+    return if(node != null){
+      true
+    }else{
+      logger.error { "Insanity found @ $name" }
+      false
+    }
   }
 
   data class InstagramWebPageInfo(
       val cursor: String,
       val hasMore: Boolean
-  )
+  ){
+    companion object {
+      val empty = InstagramWebPageInfo("",false)
+    }
+  }
 
   fun retrieveNodesForTag(tag: String, amount: Int = 100): List<InstagramWebNode> {
     val resultPair = retrieveFirstNodesForTag(tag)
